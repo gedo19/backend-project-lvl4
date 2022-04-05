8f6a0000954bb049fb34402391dfd4c58e6f8232
gedo19 <eu.MaD19@yandex.ru>
2022-04-05 22:46:27 +0600

    fix index test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,7 +18,7 @@ describe('requests', () => {
   it('GET 200', async () => {
     const res = await app.inject({
       method: 'GET',
-      url: app.reverse('root'),
+      url: app.reverse(':root'),
     });
     expect(res.statusCode).toBe(200);
   });
